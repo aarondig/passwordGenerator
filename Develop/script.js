95c4e2bc8da2bc44ca4d70e60719c8342d25a8d4
Aaron Diggdon <aarondiggdon@gmail.com>
2020-09-18 20:39:44 -0700

    Added if statements under 'upperlower' randomizer that randomly generates uppercase letters and lowercase letters

--- Develop/script.js
+++ Develop/script.js
@@ -8,65 +8,53 @@ function writePassword() {
   
 
   var chrCount = prompt ("How long would you like your password to be? (Between 8-128 characters)");
-  if (chrCount < 129) {
-    if (chrCount > 8) {
+  if (chrCount < 129 || chrCount > 8) {
+
+      var length = parseInt(chrCount);
+
+         
+        upperlower();
+          
 
 
+         function upperlower(){
+          for (var i = 0; i < length; i++) {
   
-    var length = parseInt(chrCount);
+          var charaterTypes = ["uppercase", "lowercase"];
+          var chrChoice = charaterTypes[Math.floor(Math.random() * charaterTypes.length)];
+
+          
+          if (charaterTypes="lowercase"){
+            
+            var num = Math.floor(Math.random() * 26);
+            var lowercaseChr = String.fromCharCode(97 + num);
+
+            console.log(lowercaseChr);
+           
+         }
+          if (charaterTypes="uppercase"){
+            
+            var num = Math.floor(Math.random() * 26);
+            var uppercaseChr = String.fromCharCode(65 + num);
+
+            console.log(uppercaseChr);
+         }
+        }
+          
 
 
-    var lc = lowercase();
-    var uc = uppercase();
-  
 
 
 
-
-
-    function gen(){
-    for (var i = 0; i < length; i++) {
-
-
-      var randomElement = charaterTypes[Math.floor(Math.random() * charaterTypes.length)];
-    
-      var charaterTypes = [lowercaseChr, uppercaseChr];
-    
-      var chr = Math.floor(Math.random() * charaterTypes.length);
-      console.log(random, charaterTypes[random]);
-
-    }
-
-    function lowercase(){
-     for (var i = 0; i < length; i++) {
-
-     var num = Math.floor(Math.random() * 26);
-
-     var lowercaseChr = String.fromCharCode(97 + num);
-     }
-
-    
-    }
-    function uppercase(){
-      for (var i = 0; i < length; i++) {
-      var num = Math.floor(Math.random() * 26);
-      var uppercaseChr = String.fromCharCode(65 + num);
-      }
-    }
 
    //passwordText.value = password;
 
-
-
     }
   }
   else {
     alert("Error: Invalid Input");
     }
-  }
-  else {
-    alert("Error: Invalid Input");
-  }
+  
 }
  // If statement loop
 // Add event listener to generate button
