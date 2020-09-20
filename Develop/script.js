73a91e5ac490c9d31bf9d897d0905d7719e1c408
Aaron Diggdon <aarondiggdon@gmail.com>
2020-09-19 22:24:32 -0700

    Added images to use in ReadMe

--- Develop/script.js
+++ Develop/script.js
@@ -8,39 +8,77 @@ function writePassword() {
   
 
   var chrCount = prompt ("How long would you like your password to be? (Between 8-128 characters)");
-  if (chrCount < 129 || chrCount > 8) {
-
-      var length = parseInt(chrCount);
-
-         
-        upperlower();
-          
-
-
-         function upperlower(){
-          for (var i = 0; i < length; i++) {
+  var chrLength = parseInt(chrCount);
+  if (chrCount > 128 || chrCount < 8) {
+    alert("Error: Invalid Input");
+  }
+  else {
+  var uppercase = confirm("Include uppercase letters?");
+  var lowercase = confirm("Include lowercase letters?");
+  var number = confirm("Include numbers?");
+  var special = confirm("Include special characters?");
   
-          var charaterTypes = ["uppercase", "lowercase"];
-          var chrChoice = charaterTypes[Math.floor(Math.random() * charaterTypes.length)];
-
+  
+  }
+        
+   upperlower();
           
-          if (charaterTypes="lowercase"){
+   function upperlower(){
+    var characterTypes = [];
+    if (uppercase === true){
+      characterTypes.push("uppercase");
+      }
+    if (lowercase === true){
+      characterTypes.push("lowercase");
+      }
+    if (number === true){
+      characterTypes.push("number");
+      }
+    if (special === true){
+      characterTypes.push("special");
+      }
+      document.getElementById("password").placeholder = "";
+      for (var i = 0; i < chrLength; i++) {
+        
+        var chrChoice = characterTypes[Math.floor(Math.random() * characterTypes.length)];
+          
+        if (chrChoice==="lowercase"){
             
-            var num = Math.floor(Math.random() * 26);
-            var lowercaseChr = String.fromCharCode(97 + num);
+          var num = Math.floor(Math.random() * 26);
+          var lowercaseChr = String.fromCharCode(97 + num);
+          document.getElementById("password").placeholder += lowercaseChr;
 
-            console.log(lowercaseChr);
+          console.log(lowercaseChr);
            
-         }
-          if (charaterTypes="uppercase"){
-            
-            var num = Math.floor(Math.random() * 26);
-            var uppercaseChr = String.fromCharCode(65 + num);
-
-            console.log(uppercaseChr);
-         }
         }
+        if (chrChoice==="uppercase"){
+            
+          var num = Math.floor(Math.random() * 26);
+          var uppercaseChr = String.fromCharCode(65 + num);
+          document.getElementById("password").placeholder += uppercaseChr;
+
+          console.log(uppercaseChr);
+        }
+        if (chrChoice==="number"){
+              
+          var num = Math.floor(Math.random() * 9);
+          var numberChr = String.fromCharCode(48 + num);
+          document.getElementById("password").placeholder += numberChr;
+
+          console.log(numberChr);
+        }
+        if (chrChoice==="special"){
+                
+          var num = Math.floor(Math.random() * 14);
+          var specialChr = String.fromCharCode(33 + num);
+          document.getElementById("password").placeholder += specialChr;
+          console.log(specialChr);
           
+        }
+        console.log("end");
+      }
+          
+            
 
 
 
@@ -49,13 +87,11 @@ function writePassword() {
 
    //passwordText.value = password;
 
-    }
+    
   }
-  else {
-    alert("Error: Invalid Input");
-    }
-  
 }
+
+
  // If statement loop
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
